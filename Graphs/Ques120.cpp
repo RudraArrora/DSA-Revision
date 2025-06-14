8edd2f0b32e3b28249ba59fb568fe6042e6211c1
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-14 21:54:29 +0530

    DFS

---
+++ Graphs/Ques120.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<bits/stdc++.h>
+typename<typename>T;
+class Graph{
+    map<T,list<T>>l;
+    public:
+    void addEdge(int x,int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+    void dfs_helper(T src,  map<T,bool>&visited){
+        cout<<src<<" ";
+        visited[src]=true;
+        for (auto nbr: l[src]){
+            if (!visited[nbr]){
+                dfs_helper(nbr,visited);
+            }
+        }
+    }
+    void dfs(T src){
+        map<T,bool>visited;
+        for (auto p : l){
+            T node = p.first;
+            visited[node]=false;
+        }
+    }
+
+}
+using namespace std;
+int main() {
+    cout<<"Hello World!";
+}
